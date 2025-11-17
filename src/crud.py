91a19a859841f702360d6ca2c41f89dd5483c9a8
Brantttt <danielbrant2009@gmail.com>
2025-11-17 17:39:22 -0300

    Update crud.py

---
+++ src/crud.py
@@ -0,0 +1,37 @@
+from banco import get_connection
+
+def create_aluno(aluno):
+    sql = """INSERT INTO aluno (nome, cpf, data_nascimento, status)
+    VALUES(?,?,?,?)"""
+ 
+    with get_connection() as conn:
+        cur = conn.execute(sql, (aluno.nome, aluno.cpf, aluno.data_nascimento, aluno.status))
+        return cur.lastrowid
+
+
+def read_aluno_by_id(id):
+    sql = "SELECT * FROM aluno WHERE id=?"
+    with get_connection() as conn:
+        cur = conn.execute(sql, (id,))
+        return cur.fetchone()  # Retorna None se não encontrar
+
+
+def read_aluno():
+    sql = "SELECT * FROM aluno ORDER BY id DESC"
+    with get_connection() as conn:
+        cur = conn.execute(sql)
+        return cur.fetchall()
+
+
+def update_aluno(id, novos_dados):
+    sql = "UPDATE aluno SET status = ? WHERE id = ?"
+    with get_connection() as conn:
+        cur = conn.execute(sql, (novos_dados["status"], id))
+        return cur.rowcount > 0  # True se atualizou, False se não encontrou
+
+
+def delete_aluno(id):
+    sql = "DELETE FROM aluno WHERE id=?"
+    with get_connection() as conn:
+        cur = conn.execute(sql, (id,))
+        return cur.rowcount > 0
